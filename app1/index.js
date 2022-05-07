5075aa7fdfaabc6d34f0d48862683506799ff810
MRCL <marcelo17922@gmail.com>
2022-05-07 11:34:36 -0500

    primer comit

---
+++ app1/index.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+function ventana() {
+  let principal = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  principal.loadFile("index.html");
+}
+app.whenReady().then(ventana);
+app.on("window-all-closed", function () {
+  if (process.platform === "darwin") {
+    app.quit();
+  }
+});
+app.on("activate", function () {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    ventana();
+  }
+});
